bdd73d43ecb4fbbb8cbb75db228b650af9b3a303
ansemb <31008843+ansemb@users.noreply.github.com>
2022-01-05 19:02:03 +0100

    added keybindings

--- .config/powershell/profile.ps1
+++ .config/powershell/profile.ps1
@@ -12,13 +12,38 @@ Set-PSReadLineOption -ShowToolTips
 Set-PSReadLineOption -PredictionSource History
 
 # Set the color for Prediction (auto-suggestion)
-Set-PSReadLineOption -Colors @{ InlinePrediction = '#58657a'}
-#Set-PSReadlineOption -Colors @{Prediction = 'DarkGreen' }
+Set-PSReadLineOption -Colors @{ InlinePrediction = '#6b7a9b'}
 
 
 # starship
 Invoke-Expression (&starship init powershell)
 
+
+# keybindings
+Set-PSReadlineKeyHandler -Key "ctrl+a" -ScriptBlock { [Microsoft.PowerShell.PSConsoleReadLine]::BeginningOfLine() }
+Set-PSReadlineKeyHandler -Key "ctrl+e" -ScriptBlock { [Microsoft.PowerShell.PSConsoleReadLine]::EndOfLine() }
+Set-PSReadlineKeyHandler -Key "ctrl+b" -ScriptBlock { [Microsoft.PowerShell.PSConsoleReadLine]::BackwardChar() }
+Set-PSReadlineKeyHandler -Key "alt+b" -ScriptBlock { [Microsoft.PowerShell.PSConsoleReadLine]::BackwardWord() }
+Set-PSReadlineKeyHandler -Key "ctrl+f" -ScriptBlock { [Microsoft.PowerShell.PSConsoleReadLine]::ForwardChar() }
+Set-PSReadlineKeyHandler -Key "alt+f" -ScriptBlock { [Microsoft.PowerShell.PSConsoleReadLine]::ForwardWord() }
+Set-PSReadlineKeyHandler -Key "ctrl+d" -ScriptBlock { [Microsoft.PowerShell.PSConsoleReadLine]::DeleteChar() }
+Set-PSReadlineKeyHandler -Key "ctrl+w" -ScriptBlock {
+	[Microsoft.PowerShell.PSConsoleReadLine]::EndOfLine()
+	[Microsoft.PowerShell.PSConsoleReadLine]::BackwardKillWord()
+}
+Set-PSReadlineKeyHandler -Key "ctrl+k" -ScriptBlock {
+	[Microsoft.PowerShell.PSConsoleReadLine]::ForwardDeleteLine()
+	[Microsoft.PowerShell.PSConsoleReadLine]::EndOfLine()
+}
+Set-PSReadlineKeyHandler -Key "alt+d" -ScriptBlock { [Microsoft.PowerShell.PSConsoleReadLine]::KillWord() }
+Set-PSReadlineKeyHandler -Key "alt+w" -ScriptBlock { [Microsoft.PowerShell.PSConsoleReadLine]::BackwardKillWord() }
+Set-PSReadlineKeyHandler -Key "ctrl+y" -ScriptBlock { [Microsoft.PowerShell.PSConsoleReadLine]::Yank() }
+Set-PSReadlineKeyHandler -Key "ctrl+_" -ScriptBlock { [Microsoft.PowerShell.PSConsoleReadLine]::Undo() }
+Set-PSReadlineKeyHandler -Key "ctrl+u" -ScriptBlock { [Microsoft.PowerShell.PSConsoleReadLine]::BackwardKillLine() }
+Set-PSReadlineKeyHandler -Key "ctrl+n" -ScriptBlock { [Microsoft.PowerShell.PSConsoleReadLine]::HistorySearchForward() }
+Set-PSReadlineKeyHandler -Key "ctrl+p" -ScriptBlock { [Microsoft.PowerShell.PSConsoleReadLine]::HistorySearchBackward() }
+
+Set-PSReadlineKeyHandler -Key "alt+backspace" -ScriptBlock { [Microsoft.PowerShell.PSConsoleReadLine]::BackwardKillWord() }
 
 # custom functions
 
